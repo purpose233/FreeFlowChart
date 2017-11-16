854486d927c114df6297df32ae0e936332a49c60
purpose233 <zhangsu7133@qq.com>
2017-11-16 17:54:34 +0800

    Commit before getting back.

---
+++ src/draw/ellipse.js
@@ -0,0 +1,12 @@
+function ellipseTo (context, x, y, a, b, sAngle, eAngle, counterclockwise) {
+  if (typeof sAngle === 'undefined') {
+    sAngle = 0
+  }
+  if (typeof eAngle === 'undefined') {
+    sAngle = 2 * Math.PI
+  }
+
+
+}
+
+export default ellipseTo
